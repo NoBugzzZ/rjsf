160ae6b30b55e1b32d09ea831c0bd1d3ba0cd201
NoBugzzZ <739651338@qq.com>
2021-11-16 14:10:06 +0800

    first push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Form from './core/index'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Form
+      schema={{}}
+    />
+  );
+}
+
+export default App;
